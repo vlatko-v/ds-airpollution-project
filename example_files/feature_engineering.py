98ae42a1b5fc49b1502ef7dc2663ebf13e241eeb
Vlatko <vlatko.vilovic@gmail.com>
2024-05-24 14:29:29 +0200

    air quality project vlatko

--- example_files/feature_engineering.py
+++ example_files/feature_engineering.py
@@ -20,3 +20,35 @@ def fill_missing_values(df:pd.DataFrame) -> pd.DataFrame:
     df["altitude_high_meters"] = df["altitude_high_meters"].fillna(altitude_high_meters_mean)
     df["altitude_mean_log"] = df["altitude_mean_log"].fillna(altitude_mean_log_mean)
     return df
+
+
+
+def windspeed (df:pd.DataFrame) -> pd.DataFrame:
+    df_new = df.copy()
+    df_new["windspeed"] = np.sqrt(np.power(df_new["v_component_of_wind_10m_above_ground"], 2) + np.power(df_new["u_component_of_wind_10m_above_ground"],2))
+    return df_new[["windspeed"]]
+
+
+def target_class(df:pd.DataFrame) -> pd.DataFrame:
+    df_new = df.copy()
+    df_new['PM_warning'] =df_new['target'].apply(lambda x: 1 if x >= 50 else 0)
+    return df_new[["PM_warning"]]
+
+
+def target_previous(df:pd.DataFrame) -> pd.DataFrame:
+    df_new = df.copy()
+    df_new["target_previous"] = df_new.groupby("Place_ID")["target"].shift()
+    return df_new[["target_previous"]]
+
+
+
+def forw_fill_na (df:pd.DataFrame) -> pd.DataFrame:
+    df_new = df.copy()
+    filled = df_new.fillna(method="ffill", limit=1)
+    return filled
+
+
+
+def adjusted_r_squared(r_squared, X):
+    adjusted_r2 = 1 - ((1 - r_squared) * (len(X) - 1) / (len(X) - X.shape[1] - 1))
+    return adjusted_r2
--- example_files/predict.py
+++ example_files/predict.py
@@ -30,3 +30,68 @@ X_test = fill_missing_values(X_test)
 y_test_pred = loaded_model.predict(X_test)
 mse_test = mean_squared_error(y_test, y_test_pred)
 print (f"MSE on test is: {mse_test}")
+
+
+
+def adjusted_r_squared(r_squared, X):
+    adjusted_r2 = 1 - ((1 - r_squared) * (len(X) - 1) / (len(X) - X.shape[1] - 1))
+    return adjusted_r2
+
+
+
+
+
+'''
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+# Plot formatting
+plt.style.use('fivethirtyeight')
+plt.rcParams['font.size'] = 18
+
+def evaluate_model_class(predictions, probs, train_predictions, train_probs):
+    """Compare machine learning model to baseline performance.
+    Computes statistics and shows ROC curve."""
+    
+    baseline = {}
+    
+    baseline['recall'] = r2_score(test_labels, 
+                                     [1 for _ in range(len(test_labels))])
+    baseline['precision'] = precision_score(test_labels, 
+                                      [1 for _ in range(len(test_labels))])
+    baseline['roc'] = 0.5
+    
+    results = {}
+    
+    results['recall'] = recall_score(test_labels, predictions)
+    results['precision'] = precision_score(test_labels, predictions)
+    results['roc'] = roc_auc_score(test_labels, probs)
+    
+    train_results = {}
+    train_results['recall'] = recall_score(train_labels, train_predictions)
+    train_results['precision'] = precision_score(train_labels, train_predictions)
+    train_results['roc'] = roc_auc_score(train_labels, train_probs)
+    
+    for metric in ['mean_squared_error', 'mean_absolute_error', 'r2']:
+        print(f'{metric.capitalize()} Baseline: {round(baseline[metric], 2)} Test: {round(results[metric], 2)} Train: {round(train_results[metric], 2)}')
+    
+    # Calculate false positive rates and true positive rates
+    base_fpr, base_tpr, _ = roc_curve(test_labels, [1 for _ in range(len(test_labels))])
+    model_fpr, model_tpr, _ = roc_curve(test_labels, probs)
+
+    plt.figure(figsize = (8, 6))
+    plt.rcParams['font.size'] = 16
+    
+    # Plot both curves
+    plt.plot(base_fpr, base_tpr, 'b', label = 'baseline')
+    plt.plot(model_fpr, model_tpr, 'r', label = 'model')
+    plt.legend();
+    plt.xlabel('False Positive Rate'); 
+    plt.ylabel('True Positive Rate'); plt.title('ROC Curves');
+    plt.show();
+
+evaluate_model(rf_predictions, rf_probs, train_rf_predictions, train_rf_probs)
+plt.savefig('image/nb1_roc_auc_curve.png')
+'''
